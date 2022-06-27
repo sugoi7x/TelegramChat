fc78184f4f2ea327d5ad7e063d4f1211da4cbfbd
sugoi7x <104109553+sugoi7x@users.noreply.github.com>
2022-06-27 20:23:05 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-TOKEN = os.environ.get("TOKEN", None) # Dapatkan di @botfather
-OWNER = os.environ.get("OWNER", "kenkanasw") # Isi dengan username kalian tanpa tanda @
-GROUP = os.environ.get("GROUP", "musikkugroup") # Isi dengan username group kalian tanpa tanda @ kalau gak punya gak usah isi
-CHANNEL = os.environ.get("CHANNEL", "musikkuchannel") # Isi dengan username channel kalian tanpa tanda @ kalau gak punya gak usah isi
+TOKEN = os.environ.get("TOKEN", 5343252228:AAEN_kdxqm-nul29u9cmW0MzzngZBBnek7g) # Dapatkan di @botfather
+OWNER = os.environ.get("OWNER", "Bydznesiaaa") # Isi dengan username kalian tanpa tanda @
+GROUP = os.environ.get("GROUP", "vexana0x") # Isi dengan username group kalian tanpa tanda @ kalau gak punya gak usah isi
+CHANNEL = os.environ.get("CHANNEL", "vexana0x") # Isi dengan username channel kalian tanpa tanda @ kalau gak punya gak usah isi
